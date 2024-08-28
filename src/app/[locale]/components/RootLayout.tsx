360756c5996d3c774d8e2a0f0bd69d14cab53276
s20033 <s20033@pjwstk.edu.pl>
2024-08-28 23:49:31 +0100

    update Navigation item

--- src/app/[locale]/components/RootLayout.tsx
+++ src/app/[locale]/components/RootLayout.tsx
@@ -158,24 +158,6 @@ function NavigationRow({ children }: { children: React.ReactNode }) {
   );
 }
 
-// function NavigationItem({
-//   href,
-//   children,
-// }: {
-//   href: string;
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <Link
-//       href={href}
-//       className="group relative isolate -mx-6 bg-teal-950 px-6 py-10 even:mt-px sm:mx-0 sm:px-0 sm:py-16 sm:odd:pr-16 sm:even:mt-0 sm:even:border-l sm:even:border-neutral-800 sm:even:pl-16"
-//     >
-//       {children}
-//       <span className="absolute inset-y-0 -z-10 w-screen bg-amber-700 opacity-0 transition group-odd:right-0 group-even:left-0 group-hover:opacity-100" />
-//     </Link>
-//   );
-// }
-
 function NavigationItem({
   href,
   children,
@@ -193,6 +175,8 @@ function NavigationItem({
     </Link>
   );
 }
+
+
 
 function Navigation() {
   const localActiveLanguage = useLocale();
